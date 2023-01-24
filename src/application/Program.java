b386e0e933707838bd6c813ae87c4c7c29760e76
Aires Justo <aas-justo@outlook.pt>
2023-01-24 17:12:25 +0000

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department dep = new Department(1, "Computer");
 		
-		System.out.println(dep);
+		Seller seller = new Seller(1, "Pedro Castro", "pedro@outloo.pt", sdf.parse("31/01/1976"), 1645.0, dep);
+		
+		System.out.println(seller);
 
 	}
 
