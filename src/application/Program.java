e8005b01137483a365c543c8024636b807a73c7b
Aires Justo <aas-justo@outlook.pt>
2023-01-25 15:03:35 +0000

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,9 @@ public class Program {
 		Department dep = new Department(1, "Computer");
 		
 		Seller seller = new Seller(1, "Pedro Castro", "pedro@outloo.pt", sdf.parse("31/01/1976"), 1645.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+	
 		
 		System.out.println(seller);
 
